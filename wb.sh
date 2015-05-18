1ee80772f52dbf6d6e4ea2ec9823e8a4dfe8f8b2
Levak Borok <levak92@gmail.com>
2015-05-18 20:11:50 +0200

    Fix wb.sh syntax for MSYS shell (MinGW)

--- wb.sh
+++ wb.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Usage: wb.sh [eu|na|tr]
 
 read -p "Email: " email
 read -s -p "Password: " psswd
@@ -6,13 +7,13 @@ read -s -p "Password: " psswd
 echo
 echo -n 'Connecting...'
 
-res=$(curl -s --data "email=${email}&pwd=${psswd}" \
+res=$(curl -ks --data "email=${email}&pwd=${psswd}" \
      'https://gface.com/api/-text/auth/login.json')
 
-grep 'fail' <<< $res && exit 1
+echo "$res" | grep 'fail' && exit 1
 echo 'done'
 
-token=$(sed 's/^.*token":"\([-0-9a-f]*\).*$/\1/' <<< ${res})
-userid=$(sed 's/^.*userid":\([0-9]*\).*$/\1/' <<< ${res})
+token=$(echo "$res" | sed 's/^.*token":"\([-0-9a-f]*\).*$/\1/')
+userid=$(echo "$res" | sed 's/^.*userid":\([0-9]*\).*$/\1/')
 
-./wb ${token} ${userid} eu # Europe server (others: na/tr)
+./wb ${token} ${userid} $1
