3ac96093bc51bc59eb218e68a4370685dd030982
Levak Borok <levak92@gmail.com>
2015-05-21 01:51:29 +0200

    Add forgotten commited files

---
+++ include/wb_game.h
@@ -0,0 +1,44 @@
+/**
+ * WarfaceBot, a blind XMPP client for Warface (FPS)
+ * Copyright (C) 2015 Levak Borok <levak92@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WB_GAME_H
+# define WB_GAME_H
+
+enum e_server
+{
+    SERVER_EU,
+    SERVER_NA,
+    SERVER_TR,
+    /* SERVER_BR, */
+    /* SERVER_RU, */
+    SERVER_VN,
+};
+
+# ifndef GAME_VERSION
+#  define GAME_VERSION "1.1.1.3570"
+# endif /* !GAME_VERSION */
+
+# ifndef GAME_SERVER
+#  define GAME_SERVER "com-eu.wfw.warface.com"
+# endif /* !GAME_SERVER */
+
+void game_set(enum e_server server);
+const char *game_version_get(void);
+const char *game_xmpp_server_get(void);
+
+#endif /* !WB_GAME_H */
---
+++ src/game.c
@@ -0,0 +1,73 @@
+/**
+ * WarfaceBot, a blind XMPP client for Warface (FPS)
+ * Copyright (C) 2015 Levak Borok <levak92@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <wb_game.h>
+
+static char *game_version;
+static char *game_xmpp_server;
+
+void game_set(enum e_server server)
+{
+    switch (server)
+    {
+        case SERVER_EU:
+            game_version = "1.1.1.3570";
+            game_xmpp_server = "com-eu.wfw.warface.com";
+            break;
+        case SERVER_NA:
+            game_version = "1.1.1.3570";
+            game_xmpp_server = "com-us.wfw.warface.com";
+            break;
+        case SERVER_TR:
+            game_version = "1.1.1.3570";
+            game_xmpp_server = "185.28.0.12";
+            break;
+        /*
+        case SERVER_BR:
+            game_version = TODO;
+            game_xmpp_server = TODO;
+            break;
+        case SERVER_RU:
+            game_version = TODO;
+            game_xmpp_server = "s1.warface.ru";
+            break;
+        case SERVER_CN:
+            game_version = TODO;
+            game_xmpp_server = TODO;
+            break;
+        */
+        case SERVER_VN:
+            game_version = "1.1.1.179";
+            game_xmpp_server = "rrdns.warface.goplay.vn";
+            break;
+        default:
+            game_version = GAME_VERSION;
+            game_xmpp_server = GAME_SERVER;
+            break;
+    }
+}
+
+inline const char *game_version_get(void)
+{
+    return game_version;
+}
+
+inline const char *game_xmpp_server_get(void)
+{
+    return game_xmpp_server;
+}
