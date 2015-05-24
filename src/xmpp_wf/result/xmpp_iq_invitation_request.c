f06e7e45fd15622e572e9f628f51502b7bcbaa59
DevilDaga <dagaddevil@gmail.com>
2015-05-25 04:10:55 +0530

    Print some more info on invitations

--- src/xmpp_wf/result/xmpp_iq_invitation_request.c
+++ src/xmpp_wf/result/xmpp_iq_invitation_request.c
@@ -49,13 +49,54 @@ static void xmpp_iq_invitation_request_cb(const char *msg_id,
 
     char *server = get_info(msg, "from='", "'", "Server");
     char *data = wf_get_query_content(msg);
-
+	
+/* 	<invitation_request from='Devil_Daga' ticket='12752_1608478_DevilsBitch1' room_id='12752' ms_resource='pve_11' is_follow='0' group_id='e7e423e0-6831-4a21-932a-e41bc9471b05'>
+		<game_room room_id='12752' room_type='1'>
+			<core teams_switched='0' room_name='Devil_Daga&apos;s GAME ROOM' private='1' players='1' can_start='0' team_balanced='1' revision='6'>
+				<players>
+					<player profile_id='1608478' team_id='0' status='0' observer='0' nickname='Devil_Daga' clanName='Noblesse-Oblige' class_id='0' online_id='20543777@warface/GameClient' group_id='e7e423e0-6831-4a21-932a-e41bc9471b05' presence='17' experience='132726' banner_badge='452' banner_mark='4294967295' banner_stripe='282'>
+						<skills>
+							<skill type='campaignsection3' value='0.6581485'/>
+							<skill type='easymission' value='0.6453421'/>
+							<skill type='hardmission' value='0.6575465'/>
+							<skill type='normalmission' value='0.6577018'/>
+							<skill type='trainingmission' value='0.6057903'/>
+						</skills>
+					</player>
+				</players>
+				<team_colors>
+					<team_color id='1' color='4294907157'/>
+					<team_color id='2' color='4279655162'/>
+				</team_colors>
+			</core>
+			<session id='' status='0' game_progress='0' start_time='18446744011573954816' revision='2'/>
+			<custom_params friendly_fire='0' enemy_outlines='1' auto_team_balance='0' dead_can_chat='1' join_in_the_process='1' max_players='5' class_restriction='253' inventory_slot='2113929215' revision='4'/>
+			<mission mission_key='d1cac6df-53e5-4c87-98cf-af8490d16cfb' no_teams='1' name='@na_mission_path01_1' setting='africa/africa_base' mode='pve' mode_name='@PvE_game_mode_desc' mode_icon='pve_icon' description='@mission_desc_africa_path' image='mapImgAfrica' difficulty='easy' type='trainingmission' time_of_day='9:06' revision='4'>
+				<objectives factor='1'>
+					<objective id='0' type='primary'/>
+					<objective id='12' type='secondary'/>
+				</objectives>
+			</mission>
+			<pings revision='1'/>
+			<kick_vote_params success='0.6' timeout='60' cooldown='300' revision='1'/>
+			<room_master master='1608478' revision='4'/>
+		</game_room>
+	</invitation_request>
+ */
+	
     if (!data)
-        return;
+    {
+		if(server)
+			free(server);
+		return;
+	}
 
-    char *resource = get_info(data, "ms_resource='", "'", "Resource");
-    char *ticket = get_info(data, "ticket='", "'", "Ticket");
+	get_info(data, "invitation_request from='", "'", "INVITER");
+    char *resource = get_info(data, "ms_resource='", "'", NULL);
+    char *ticket = get_info(data, "ticket='", "'", NULL);
     char *room = get_info(data, "room_id='", "'", "Room ID");
+	get_info(data, "difficulty='", "'", "DIFFICULTY" );
+	get_info(data, "room_name='", "'", "ROOM NAME" );
 
     if (server && resource && ticket && room)
     {
