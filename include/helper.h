3c73a86296958898a43e9584f988b789cb59237f
DevilDaga <dagaddevil@gmail.com>
2015-05-31 00:27:17 +0530

    Revert "Revert "Fix crash due to second 'hello' in room""
    This reverts commit c07f8a8eb6bb85b4949c60ace468a783c0001772.

--- include/helper.h
+++ include/helper.h
@@ -1,5 +1,6 @@
-#ifndef STRREPLACE
-#define STRREPLACE
+#ifndef _HELPER_
+#define _HELPER_
+#include <textcolor.h>
 
 char *get_timestamp ( );
 
@@ -10,5 +11,10 @@ char *str_replace ( const char *orig, const char *rep, const char *with );
 int levenshtein(const char *s1, const char *s2);
 
 char *name_in_string( char* str, const char* name, int percentage);
+
+#define	LOGPRINT(fmt, ...)	do {								\
+			printf( KWHT BOLD"[%s]  "KRST, get_timestamp ( ));	\
+			printf( fmt, __VA_ARGS__);							\
+		} while (0)
 
 #endif
