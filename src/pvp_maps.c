d60d663b13af68f68a5a47f684482838bc53829e
Levak Borok <levak92@gmail.com>
2019-12-15 17:27:05 +0100

    Add `tdm_ghost_town_xmas` PvP map

--- src/pvp_maps.c
+++ src/pvp_maps.c
@@ -109,6 +109,7 @@ void pvp_maps_add_to_list(struct list *ml)
     add(ml, "tdm_farm", "30afabfd-032f-4ff3-a22e-8b889349aaa5");
     add(ml, "tdm_grand_bazaar", "3347d361-ef36-4c57-8bca-4f8051fc97e6");
     add(ml, "tdm_ghost_town", "b4ac3d2f-1e97-4132-800e-7e1dbcd3512f");
+    add(ml, "tdm_ghost_town_xmas", "ad7e9b12-7c47-428a-860b-21c5260ce6bd");
     add(ml, "tdm_hangar_up", "e5981b6a-325d-42eb-a3fe-e6eed0bc4bf2");
     add(ml, "tdm_hangar_OG16", "bf506d56-21cd-49e3-b57a-4b1e6f66ba8d");
     add(ml, "tdm_hangar_xmas", "953740a3-022e-4243-9044-eb1c07a2f680");
